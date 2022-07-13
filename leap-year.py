6769d191dd79de25d6db3bb2075e17e7ca692ae6
Mp <mehmetpeker0205@gmail.com>
2022-07-13 14:21:45 +0300

    that is changed with which

--- leap-year.py
+++ leap-year.py
@@ -4,7 +4,8 @@
 
 # **Exercise | ?comprehension | ?range() | ?len() | ?nested for loop() | ?any() | ?sum() | ?append()**
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+# What is the smallest positive number which  is evenly divisible by all of the numbers from 1 to 20?
+
 nums_prime = []
 
 for i in nums :
